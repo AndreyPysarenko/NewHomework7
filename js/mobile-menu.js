41c8be24d50f0f2cd02e5dddc82d5ff26da0eda8
AndreyPysarenko <114475908+AndreyPysarenko@users.noreply.github.com>
2023-04-27 13:42:33 +0300

    update

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,18 +1,12 @@
-(() => {
+
     const mobileMenu = document.querySelector('.js-menu-container');
     const openMenuBtn = document.querySelector('.js-open-menu');
     const closeMenuBtn = document.querySelector('.js-close-menu');
+    const body = document.querySelector('body')
   
-    const toggleMenu = () => {
-      const isMenuOpen =
-        openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-      openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    const toggleMenu = () => {          
       mobileMenu.classList.toggle('is-open');
-  
-      const scrollLockMethod = !isMenuOpen
-        ? 'disableBodyScroll'
-        : 'enableBodyScroll';
-      bodyScrollLock[scrollLockMethod](document.body);
+      body.classList.toggle('no-scroll');
     };
   
     openMenuBtn.addEventListener('click', toggleMenu);
@@ -22,8 +16,7 @@
     window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
       if (!e.matches) return;
       mobileMenu.classList.remove('is-open');
-      openMenuBtn.setAttribute('aria-expanded', false);
-      bodyScrollLock.enableBodyScroll(document.body);
+    body.classList.remove('no-scroll');
     });
-  })();
+
   
